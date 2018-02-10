bdeecc77d65d2b7894fb5c2f4964110217a88891
sanjay <sgupt9999@gmail.com>
2018-02-10 23:03:37 +0000

    Add ssh password authentication to ec2

---
+++ sshpasswordauth.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+#Changing ec2-user password to redhat
+echo "redhat" | sudo passwd ec2-user --stdin
+sudo systemctl reload sshd
