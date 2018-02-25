5e35d58689d514722afccf9dfce8cada2956ea60
Cloud User <ec2-user@ip-172-31-23-0.us-west-2.compute.internal>
2018-02-25 18:01:23 +0000

    Added comment

--- sshpasswordauth.sh
+++ sshpasswordauth.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Adding SSH password authentication to Centos/RHAT 7 
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 #Changing ec2-user password to redhat
 echo "redhat" | sudo passwd ec2-user --stdin
