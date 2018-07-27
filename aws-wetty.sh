c9926d843f47a6551f073cde785b9647186f77b9
Cloud User <ec2-user@ip-172-31-29-203.us-west-2.compute.internal>
2018-07-27 15:25:11 +0000

    Checking in aws-wetty. Still being tested

--- aws-wetty.sh
+++ aws-wetty.sh
@@ -1,33 +1,54 @@
 #!/bin/bash
-#Install nginx and wetty on RHEL 7 for http login using port 8888
-#These instructions are from this video on youtube - https://www.youtube.com/watch?v=gjHYl81t2bI
-
-
-# User input
+#################################################################################################
+# This script will install nginx and wetty on RHEL 7 for weblogin incase ssh is blocked
+# These instructions are from this video on youtube - https://www.youtube.com/watch?v=gjHYl81t2bI
+#################################################################################################
+# Start of user inputs
 SETUSERPASSWD="no"
 #SETUSERPASSWD="yes"
 USERNAME="ec2-user"
 PASSWORD="redhat"
 LOGINPORT=80
 # End of user input
+#################################################################################################
 
-PACKAGES="nginx git gcc gcc-c++ openssl-devel npm nodejs"
+INSTALLPACKAGES="nginx git gcc gcc-c++ openssl-devel npm nodejs"
 
 if [[ $EUID != "0" ]]
 then
-	echo "ERROR. Need to have root privileges to run this script"
+	echo
+	echo "##########################################################"
+	echo "ERROR. You need to have root privileges to run this script"
+	echo "##########################################################"
 	exit 1
+else
+	echo
+	echo "############################################################"
+	echo "This script will enable weblogin on this server on port $LOGINPORT"
+	echo "############################################################"
+	sleep 5
 fi
 
 
-
+# Install epel repo
 cd /tmp
 yum install -y -q wget
 wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum install -y -q epel-release-latest-7.noarch.rpm
 rm -rf epel-release-latest-7.noarch.rpm
-#yum install nginx git gcc gcc-c++ openssl-devel -y
-yum install -y -q $PACKAGES
+
+
+echo
+echo "###################################################"
+echo "Installing packages $INSTALLPACKAGES"
+yum install -y -q $INSTALLPACKAGES > /dev/null 2>&1
+echo "Done"
+echo "###################################################"
+
+
+echo
+echo "#####################################################"
+echo "Configuring npm"
 wget https://nodejs.org/dist/v8.9.4/node-v8.9.4.tar.gz
 mv node-v8.9.4.tar.gz /usr/local/src/
 cd /usr/local/src
@@ -38,15 +59,12 @@ cd node-v8.9.4
 # The following make can take more than an hour to complete
 make
 make install
-#yum -y install npm nodejs
-#chmod o+x /root
-# End of make
-
-
 cd /usr/local/src
 git clone https://github.com/krishnasrinivas/wetty
 cd wetty
 npm install -y
+echo "Configuration complete"
+echo "#####################################################"
 
 #Setting the password for weblogin
 if [[ $SETUSERPASSWD == "yes" ]]
@@ -54,7 +72,14 @@ then
 	echo "$PASSWORD" | passwd $USERNAME --stdin
 fi
 
-node app.js -p $PORT &
+node app.js -p $LOGINPORT &
 
+
+echo
+echo "#########################################"
 echo `date`
+echo "Weblogin Installation complete"
+echo -n "For weblogin- http://" && curl http://ident.me && echo ":80" && echo
+echo "Done"
+echo "#########################################"
 echo "Initialization Complete"
